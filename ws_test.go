b8f7fa0dacdca8fadb494b32280113891590c86c
Jeromy <why@ipfs.io>
2016-05-16 12:29:11 -0700

    first working commit

---
+++ ws_test.go
@@ -0,0 +1,65 @@
+package websocket
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	ma "github.com/jbenet/go-multiaddr"
+)
+
+var _ = log.Println
+
+func TestMultiaddrParsing(t *testing.T) {
+	addr, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/5555/ws")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = parseMultiaddr(addr)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestWebsocketListen(t *testing.T) {
+	zero, err := ma.NewMultiaddr("/ip4/127.0.0.1/tcp/0/ws")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	tpt := &WebsocketTransport{}
+	l, err := tpt.Listen(zero)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	go func() {
+		d, _ := tpt.Dialer(nil)
+		c, err := d.Dial(l.Multiaddr())
+		if err != nil {
+			t.Error(err)
+			return
+		}
+
+		c.Write([]byte("HELLO WORLD!"))
+		time.Sleep(time.Second)
+		c.Close()
+	}()
+
+	c, err := l.Accept()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	buf := make([]byte, 32)
+	n, err := c.Read(buf)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	log.Printf("READ: %s", buf[:n])
+	c.Close()
+	l.Close()
+	time.Sleep(time.Second)
+}
