35be93ae1502b5a2193740d7719ba8a5b937ad70
dignifiedquire <dignifiedquire@gmail.com>
2017-07-09 21:47:26 +0200

    more tests

--- addrs.go
+++ addrs.go
@@ -7,8 +7,28 @@ import (
 
 	ma "github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr-net"
-	ws "golang.org/x/net/websocket"
 )
+
+// Addr is an implementation of net.Addr for WebSocket.
+type Addr struct {
+	*url.URL
+}
+
+var _ net.Addr = (*Addr)(nil)
+
+// Network returns the network type for a WebSocket, "websocket".
+func (addr *Addr) Network() string {
+	return "websocket"
+}
+
+// NewAddr creates a new Addr using the given host string
+func NewAddr(host string) *Addr {
+	return &Addr{
+		URL: &url.URL{
+			Host: host,
+		},
+	}
+}
 
 func ConvertWebsocketMultiaddrToNetAddr(maddr ma.Multiaddr) (net.Addr, error) {
 	_, host, err := manet.DialArgs(maddr)
@@ -16,16 +36,11 @@ func ConvertWebsocketMultiaddrToNetAddr(maddr ma.Multiaddr) (net.Addr, error) {
 		return nil, err
 	}
 
-	a := &ws.Addr{
-		URL: &url.URL{
-			Host: host,
-		},
-	}
-	return a, nil
+	return NewAddr(host), nil
 }
 
 func ParseWebsocketNetAddr(a net.Addr) (ma.Multiaddr, error) {
-	wsa, ok := a.(*ws.Addr)
+	wsa, ok := a.(*Addr)
 	if !ok {
 		return nil, fmt.Errorf("not a websocket address")
 	}
