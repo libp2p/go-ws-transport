fcab63639468cca8f32ea0e5b6c6fc454eb6bd2e
Alex Browne <stephenalexbrowne@gmail.com>
2019-08-07 18:34:15 -0700

    Implement bare bones Wasm support (needs to be cleaned up)

--- go.mod
+++ go.mod
@@ -1,6 +1,7 @@
 module github.com/libp2p/go-ws-transport
 
 require (
+	github.com/0xProject/qunit v0.0.0-20190730000255-81c18fdf7752
 	github.com/gorilla/websocket v1.4.0
 	github.com/libp2p/go-libp2p-core v0.0.1
 	github.com/libp2p/go-libp2p-mplex v0.2.0
@@ -8,5 +9,6 @@ require (
 	github.com/libp2p/go-libp2p-transport-upgrader v0.1.1
 	github.com/multiformats/go-multiaddr v0.0.4
 	github.com/multiformats/go-multiaddr-net v0.0.1
+	github.com/norunners/vert v0.0.0-20190616030331-d9c8c2aacdf1
 	github.com/whyrusleeping/mafmt v1.2.8
 )
--- websocket.go
+++ websocket.go
@@ -4,16 +4,10 @@ package websocket
 import (
 	"context"
 	"fmt"
-	"net"
-	"net/http"
-	"net/url"
 
 	"github.com/libp2p/go-libp2p-core/peer"
 	"github.com/libp2p/go-libp2p-core/transport"
-
 	tptu "github.com/libp2p/go-libp2p-transport-upgrader"
-
-	ws "github.com/gorilla/websocket"
 	ma "github.com/multiformats/go-multiaddr"
 	manet "github.com/multiformats/go-multiaddr-net"
 	mafmt "github.com/whyrusleeping/mafmt"
@@ -37,14 +31,6 @@ var WsCodec = &manet.NetCodec{
 	ParseNetAddr:     ParseWebsocketNetAddr,
 }
 
-// Default gorilla upgrader
-var upgrader = ws.Upgrader{
-	// Allow requests from *all* origins.
-	CheckOrigin: func(r *http.Request) bool {
-		return true
-	},
-}
-
 func init() {
 	err := ma.AddProtocol(WsProtocol)
 	if err != nil {
@@ -54,6 +40,8 @@ func init() {
 	manet.RegisterNetCodec(WsCodec)
 }
 
+var _ transport.Transport = (*WebsocketTransport)(nil)
+
 // WebsocketTransport is the actual go-libp2p transport
 type WebsocketTransport struct {
 	Upgrader *tptu.Upgrader
@@ -62,8 +50,6 @@ type WebsocketTransport struct {
 func New(u *tptu.Upgrader) *WebsocketTransport {
 	return &WebsocketTransport{u}
 }
-
-var _ transport.Transport = (*WebsocketTransport)(nil)
 
 func (t *WebsocketTransport) CanDial(a ma.Multiaddr) bool {
 	return WsFmt.Matches(a)
@@ -77,84 +63,10 @@ func (t *WebsocketTransport) Proxy() bool {
 	return false
 }
 
-func (t *WebsocketTransport) maDial(ctx context.Context, raddr ma.Multiaddr) (manet.Conn, error) {
-	wsurl, err := parseMultiaddr(raddr)
-	if err != nil {
-		return nil, err
-	}
-
-	wscon, _, err := ws.DefaultDialer.Dial(wsurl, nil)
-	if err != nil {
-		return nil, err
-	}
-
-	mnc, err := manet.WrapNetConn(NewConn(wscon))
-	if err != nil {
-		wscon.Close()
-		return nil, err
-	}
-	return mnc, nil
-}
-
 func (t *WebsocketTransport) Dial(ctx context.Context, raddr ma.Multiaddr, p peer.ID) (transport.CapableConn, error) {
 	macon, err := t.maDial(ctx, raddr)
 	if err != nil {
 		return nil, err
 	}
 	return t.Upgrader.UpgradeOutbound(ctx, t, macon, p)
-}
-
-func (t *WebsocketTransport) maListen(a ma.Multiaddr) (manet.Listener, error) {
-	lnet, lnaddr, err := manet.DialArgs(a)
-	if err != nil {
-		return nil, err
-	}
-
-	nl, err := net.Listen(lnet, lnaddr)
-	if err != nil {
-		return nil, err
-	}
-
-	u, err := url.Parse("http://" + nl.Addr().String())
-	if err != nil {
-		nl.Close()
-		return nil, err
-	}
-
-	malist, err := t.wrapListener(nl, u)
-	if err != nil {
-		nl.Close()
-		return nil, err
-	}
-
-	go malist.serve()
-
-	return malist, nil
-}
-
-func (t *WebsocketTransport) Listen(a ma.Multiaddr) (transport.Listener, error) {
-	malist, err := t.maListen(a)
-	if err != nil {
-		return nil, err
-	}
-	return t.Upgrader.UpgradeListener(t, malist), nil
-}
-
-func (t *WebsocketTransport) wrapListener(l net.Listener, origin *url.URL) (*listener, error) {
-	laddr, err := manet.FromNetAddr(l.Addr())
-	if err != nil {
-		return nil, err
-	}
-	wsma, err := ma.NewMultiaddr("/ws")
-	if err != nil {
-		return nil, err
-	}
-	laddr = laddr.Encapsulate(wsma)
-
-	return &listener{
-		laddr:    laddr,
-		Listener: l,
-		incoming: make(chan *Conn),
-		closed:   make(chan struct{}),
-	}, nil
 }
