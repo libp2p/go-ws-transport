5dcc4d93fe95f4e5b34b315fee7b74732aed05b0
Marten Seemann <martenseemann@gmail.com>
2022-05-25 20:36:04 +0200

    deprecate this repo (#119)
    * deprecate this repo
    
    * release v0.7.0

--- addrs.go
+++ addrs.go
@@ -1,26 +1,16 @@
 package websocket
 
 import (
-	"fmt"
 	"net"
-	"net/url"
-	"strconv"
+
+	"github.com/libp2p/go-libp2p/p2p/transport/websocket"
 
 	ma "github.com/multiformats/go-multiaddr"
-	manet "github.com/multiformats/go-multiaddr/net"
 )
 
 // Addr is an implementation of net.Addr for WebSocket.
-type Addr struct {
-	*url.URL
-}
-
-var _ net.Addr = (*Addr)(nil)
-
-// Network returns the network type for a WebSocket, "websocket".
-func (addr *Addr) Network() string {
-	return "websocket"
-}
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/transport/websocket.Addr instead.
+type Addr = websocket.Addr
 
 // NewAddr creates an Addr with `ws` scheme (insecure).
 //
@@ -33,105 +23,17 @@ func NewAddr(host string) *Addr {
 
 // NewAddrWithScheme creates a new Addr using the given host string. isSecure
 // should be true for WSS connections and false for WS.
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/transport/websocket.NewAddrWithScheme instead.
 func NewAddrWithScheme(host string, isSecure bool) *Addr {
-	scheme := "ws"
-	if isSecure {
-		scheme = "wss"
-	}
-	return &Addr{
-		URL: &url.URL{
-			Scheme: scheme,
-			Host:   host,
-		},
-	}
+	return websocket.NewAddrWithScheme(host, isSecure)
 }
 
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/transport/websocket.ConvertWebsocketMultiaddrToNetAddr instead.
 func ConvertWebsocketMultiaddrToNetAddr(maddr ma.Multiaddr) (net.Addr, error) {
-	url, err := parseMultiaddr(maddr)
-	if err != nil {
-		return nil, err
-	}
-	return &Addr{URL: url}, nil
+	return websocket.ConvertWebsocketMultiaddrToNetAddr(maddr)
 }
 
+// Deprecated: use github.com/libp2p/go-libp2p/p2p/transport/websocket.ParseWebsocketNetAddr instead.
 func ParseWebsocketNetAddr(a net.Addr) (ma.Multiaddr, error) {
-	wsa, ok := a.(*Addr)
-	if !ok {
-		return nil, fmt.Errorf("not a websocket address")
-	}
-
-	var (
-		tcpma ma.Multiaddr
-		err   error
-		port  int
-		host  = wsa.Hostname()
-	)
-
-	// Get the port
-	if portStr := wsa.Port(); portStr != "" {
-		port, err = strconv.Atoi(portStr)
-		if err != nil {
-			return nil, fmt.Errorf("failed to parse port '%q': %s", portStr, err)
-		}
-	} else {
-		return nil, fmt.Errorf("invalid port in url: '%q'", wsa.URL)
-	}
-
-	// NOTE: Ignoring IPv6 zones...
-	// Detect if host is IP address or DNS
-	if ip := net.ParseIP(host); ip != nil {
-		// Assume IP address
-		tcpma, err = manet.FromNetAddr(&net.TCPAddr{
-			IP:   ip,
-			Port: port,
-		})
-		if err != nil {
-			return nil, err
-		}
-	} else {
-		// Assume DNS name
-		tcpma, err = ma.NewMultiaddr(fmt.Sprintf("/dns/%s/tcp/%d", host, port))
-		if err != nil {
-			return nil, err
-		}
-	}
-
-	wsma, err := ma.NewMultiaddr("/" + wsa.Scheme)
-	if err != nil {
-		return nil, err
-	}
-
-	return tcpma.Encapsulate(wsma), nil
-}
-
-func parseMultiaddr(maddr ma.Multiaddr) (*url.URL, error) {
-	// Only look at the _last_ component.
-	maddr, wscomponent := ma.SplitLast(maddr)
-	if maddr == nil || wscomponent == nil {
-		return nil, fmt.Errorf("websocket addrs need at least two components")
-	}
-
-	var scheme string
-	switch wscomponent.Protocol().Code {
-	case ma.P_WS:
-		scheme = "ws"
-	case ma.P_WSS:
-		scheme = "wss"
-	default:
-		return nil, fmt.Errorf("not a websocket multiaddr")
-	}
-
-	network, host, err := manet.DialArgs(maddr)
-	if err != nil {
-		return nil, err
-	}
-	switch network {
-	case "tcp", "tcp4", "tcp6":
-	default:
-		return nil, fmt.Errorf("unsupported websocket network %s", network)
-	}
-	return &url.URL{
-		Scheme: scheme,
-		Host:   host,
-	}, nil
+	return websocket.ParseWebsocketNetAddr(a)
 }
--- go.mod
+++ go.mod
@@ -1,49 +1,37 @@
 module github.com/libp2p/go-ws-transport
 
 require (
-	github.com/gorilla/websocket v1.4.2
-	github.com/libp2p/go-conn-security-multistream v0.3.0
-	github.com/libp2p/go-libp2p-core v0.14.0
-	github.com/libp2p/go-libp2p-mplex v0.5.0
-	github.com/libp2p/go-libp2p-testing v0.7.0
-	github.com/libp2p/go-libp2p-transport-upgrader v0.7.0
-	github.com/multiformats/go-multiaddr v0.4.1
-	github.com/multiformats/go-multiaddr-fmt v0.1.0
-	github.com/stretchr/testify v1.7.0
+	github.com/gorilla/websocket v1.5.0
+	github.com/libp2p/go-libp2p v0.20.0
+	github.com/libp2p/go-libp2p-core v0.16.1
+	github.com/multiformats/go-multiaddr v0.5.0
 )
 
 require (
-	github.com/btcsuite/btcd v0.20.1-beta // indirect
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/davidlazar/go-crypto v0.0.0-20170701192655-dcfb0a7ac018 // indirect
-	github.com/gogo/protobuf v1.3.1 // indirect
-	github.com/ipfs/go-cid v0.0.7 // indirect
-	github.com/ipfs/go-log/v2 v2.5.0 // indirect
-	github.com/jbenet/go-temp-err-catcher v0.1.0 // indirect
+	github.com/btcsuite/btcd v0.22.1 // indirect
+	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
+	github.com/btcsuite/btcd/chaincfg/chainhash v1.0.1 // indirect
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/ipfs/go-cid v0.1.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/libp2p/go-buffer-pool v0.0.2 // indirect
-	github.com/libp2p/go-libp2p-pnet v0.2.0 // indirect
-	github.com/libp2p/go-mplex v0.4.0 // indirect
-	github.com/libp2p/go-msgio v0.0.6 // indirect
 	github.com/libp2p/go-openssl v0.0.7 // indirect
-	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/minio/blake2b-simd v0.0.0-20160723061019-3f5f724cb5b1 // indirect
-	github.com/minio/sha256-simd v0.1.1 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
 	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/multiformats/go-base32 v0.0.3 // indirect
 	github.com/multiformats/go-base36 v0.1.0 // indirect
+	github.com/multiformats/go-multiaddr-fmt v0.1.0 // indirect
 	github.com/multiformats/go-multibase v0.0.3 // indirect
-	github.com/multiformats/go-multihash v0.0.14 // indirect
-	github.com/multiformats/go-multistream v0.2.1 // indirect
+	github.com/multiformats/go-multicodec v0.4.1 // indirect
+	github.com/multiformats/go-multihash v0.1.0 // indirect
 	github.com/multiformats/go-varint v0.0.6 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spacemonkeygo/spacelog v0.0.0-20180420211403-2296661a0572 // indirect
 	github.com/spaolacci/murmur3 v1.1.0 // indirect
-	go.uber.org/atomic v1.7.0 // indirect
-	go.uber.org/multierr v1.6.0 // indirect
-	go.uber.org/zap v1.19.1 // indirect
-	golang.org/x/crypto v0.0.0-20191011191535-87dc89f01550 // indirect
-	golang.org/x/sys v0.0.0-20210630005230-0f9fa26af87c // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
+	golang.org/x/sys v0.0.0-20220422013727-9388b58f7150 // indirect
+	lukechampine.com/blake3 v1.1.6 // indirect
 )
 
 go 1.17
